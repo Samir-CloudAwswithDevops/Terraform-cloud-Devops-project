bd40215b8a155f2c62809084c1b65dbc2359dd20
Samir-CloudAwswithDevops <samirkumargouda0563@gmail.com>
2025-10-30 15:18:08 +0530

    enter the value Terraform-day-4-statelockingfiles

--- Terraform-day-4-statelockingfiles/backend.tf
+++ Terraform-day-4-statelockingfiles/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucketwww"   # your actual bucket name
-    key    = "day-4 locking files terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "terraform-bucketwww"   # Your actual S3 bucket name
+    key            = "day-4/terraform.tfstate"          # Path inside the bucket
+    region         = "us-east-1"                       # S3 region
+    dynamodb_table = "samir"                            # DynamoDB table for state locking
+    encrypt        = true                              # Encrypt state file in S3
   }
 }
