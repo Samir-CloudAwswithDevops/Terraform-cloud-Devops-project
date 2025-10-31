ec55073be3c4f3a0710b44dcd2223abf6b548fb5
Samir-CloudAwswithDevops <samirkumargouda0563@gmail.com>
2025-10-31 17:42:23 +0530

    enter value day-5-lifecycle-rules/

---
+++ day-5-lifecycle-rules/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "name" {
+   # ami = "ami-07860a2d7eb515d9a"
+    ami = "ami-0bdd88bd06d16ba03"
+    instance_type = "t3.micro"
+    tags = {
+      Name = "lifecycle-rules"
+    }
+
+    lifecycle {
+      create_before_destroy = true
+    }
+    
+    # lifecycle {
+    #   ignore_changes = [tags,  ]
+    # }
+    # lifecycle {
+    #   prevent_destroy = true
+    # }
+  
+}
